ab42d500fabc064e1d1f61f729f159828d8e6f22
Logan Murphy <lmurphy@learnvest.com>
2018-11-30 17:08:26 -0500

    improve audit

--- src/js/data/accomplishments.jsx
+++ src/js/data/accomplishments.jsx
@@ -25,9 +25,9 @@ export default [{
   }]
 }, {
   name: 'Amazon Web Services',
-  logo: <img src={AWSCertifiedLogo} height='48' />,
+  logo: <img alt="AWS" src={AWSCertifiedLogo} height='48' />,
   links: [{
     url: AWSCertifiedPDF,
-    text: <img src={AWSCertifiedTag} />
+    text: <img src={AWSCertifiedTag} alt="AWS Certified Developer Associate" />
   }]
 }]
