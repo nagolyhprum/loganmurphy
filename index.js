46f47c80f7d45785d21cb8a7fd930b7448d53375
Logan Murphy <logan@medium.com>
2021-04-20 12:31:33 -0400

    use the correct port

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ var express = require("express");
 var static = require("express-static");
 var path = require("path");
 var app = express();
+var port = process.env.PORT || 80;
 app.use(static(path.join(__dirname, "dist"), {}))
-app.listen(80, function() {
-    console.log("listening on port 80");
+app.listen(port, function() {
+    console.log("listening on port", port);
 })
